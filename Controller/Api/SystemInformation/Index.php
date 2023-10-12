d4497165dc093d11b10fea247dc43ed3d4f3fea2
adeelq <adeel.qamar@apsis.com>
2023-10-12 09:19:49 +0200

    changes to system information endpoint

--- Controller/Api/SystemInformation/Index.php
+++ Controller/Api/SystemInformation/Index.php
@@ -4,7 +4,11 @@ namespace Apsis\One\Controller\Api\SystemInformation;
 
 use Apsis\One\Controller\Api\AbstractApi;
 use Apsis\One\Service\BaseService;
+use Magento\Customer\Model\CustomerFactory;
+use Magento\Framework\App\RequestInterface;
 use Magento\Framework\App\ResponseInterface;
+use Magento\Framework\Encryption\EncryptorInterface;
+use Magento\Framework\App\ProductMetadataInterface;
 use Throwable;
 
 class Index extends AbstractApi
@@ -40,16 +44,41 @@ class Index extends AbstractApi
     ];
 
     /**
+     * @var ProductMetadataInterface
+     */
+    private ProductMetadataInterface $productMetadata;
+
+    /**
+     * @param RequestInterface $request
+     * @param ResponseInterface $response
+     * @param BaseService $service
+     * @param CustomerFactory $customerFactory
+     * @param EncryptorInterface $encryptor
+     * @param ProductMetadataInterface $productMetadata
+     */
+    public function __construct(
+        RequestInterface $request,
+        ResponseInterface $response,
+        BaseService $service,
+        CustomerFactory $customerFactory,
+        EncryptorInterface $encryptor,
+        ProductMetadataInterface $productMetadata
+    ) {
+        $this->productMetadata = $productMetadata;
+        parent::__construct($request, $response, $service, $customerFactory, $encryptor);
+    }
+
+    /**
      * @return ResponseInterface
      */
     protected function getSystemInformation(): ResponseInterface
     {
         $info = [
-            'system_id' => 'magento',
+            'system_id' => 'adobe-commerce',
             'system_access_url' => $this->service->generateSystemAccessUrl($this->getRequest()),
-            'system_version' => $this->service->getCurrentVersion(),
-            'supported_integration_versions' => ['v2.0'],
-            'supported_features' => ['PROFILE_SYNC', 'PROFILE_LIST_SYNC', 'UTILIZES_ONE_API', 'ECOMMERCE_EVENTS'],
+            'system_version' => $this->productMetadata->getVersion(),
+            'supported_integration_versions' => ['v3.0'],
+            'supported_features' => ['PROFILE_SYNC', 'PROFILE_LIST_SYNC'],
             'supported_marketing_automation_nodes' => []
         ];
         return $this->sendResponse(200, null, json_encode($info));
